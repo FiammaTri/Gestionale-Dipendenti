15592a0bf22b71749eb72d0cb2368cb943b57478
FiammaTri <scinty-pcd@live.it>
2025-01-13 17:52:14 +0100

    Aggiunti metodi nella classe Database e Employee

--- GestionaleDipendenti/src/Gestionale/Main.java
+++ GestionaleDipendenti/src/Gestionale/Main.java
@@ -1,10 +1,16 @@
 package Gestionale;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-
+		Scanner scanner = new Scanner(System.in);
+		//Database.createTables(); //FUNZIONA FINO A CREAZIONEDEVELOPER. BISOGNA INVERTIRE L'ORDINE E METTERE PRIMA CREAZIONE TEAM
+		Employee.aggiuntaDipendente(scanner); //FUNZIONA
+		//Employee.eliminazioneDipendente(scanner); //DA VERIFICARE
+		Employee.stampaDipendente(scanner);
+		scanner.close();
 	}
 
 }
